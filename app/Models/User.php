8b4f6a20386e6cc2f46d9814703f0a5f97791f79
alifanLeywin <alifanmunggaran16@gmail.com>
2025-06-05 07:58:30 +0700

    modeluser

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,46 +2,30 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable; // penting untuk fitur auth
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    // karena nama tabel bukan 'users', harus ditentukan
+    protected $table = 'user';
+
     protected $fillable = [
-        'name',
+        'nama_lengkap',
+        'username',
+        'nomor_wa',
         'email',
         'password',
+        'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    public $timestamps = true;
 }
